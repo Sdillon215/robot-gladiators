829090e7788202341279b38868f592e566bc1501
Sean <sdillon215@gmail.com>
2021-06-22 22:17:44 -0700

    playerName prompt added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,11 @@
-window.alert("This is an alert!");
+// prompt to ask for playerName and store that value in playerName variable
+var playerName = window.prompt("What is your players name?");
+
+console.log(playerName);
+
+// this creates a function named fight
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
